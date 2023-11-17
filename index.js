755c7ca5790426a11d3653d96c25747ee46074fc
FrancaFigueroa <francaafigueroa@gmail.com>
2023-11-17 18:56:39 -0300

    navMenu responsive

---
+++ index.js
@@ -0,0 +1,4 @@
+const navToggle = document.querySelector(".navToggle")
+const navMenu = document.querySelector(".navMenu")
+
+navToggle.addEventListener("click", () => {navMenu.classList.toggle("navMenuVisible")});
